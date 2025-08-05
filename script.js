59e444535fdc0bdbb77a8ba45608dd91403c8896
Odilio <odilionoronha@gmail.com>
2025-08-05 16:54:17 -0300

    Add/update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function showAlert() {
+    alert('Hello from Example Template!');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Example template loaded successfully!');
+});
